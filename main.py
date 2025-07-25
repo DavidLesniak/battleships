cb15611120f7a8de7880418b25ba69c73682187a
DavidLesniak <dawid.lesniak01@outlook.com>
2025-07-25 20:58:38 +0200

    Poprawa błedów pętli głównej

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pygame as pg
 
+
 pg.init()
 display = pg.display.set_mode((1200, 600))
 clock = pg.time.Clock()
 
-if name == 'main':
-    run = True
 
-    while run:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                run = False
 
-        pg.display.flip()
-        clock.tick(60)
+run = True
 
-    pg.quit()
+while run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+
+    pg.display.flip()
+    clock.tick(60)
+
+pg.quit()
