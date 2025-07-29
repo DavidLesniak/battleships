3ee13ee2c11fa5c2a004c9aa2e68bf2383bdb0c8
Tomerson333 <coca.cola990@spoko.pl>
2025-07-29 13:46:44 +0200

    dodadnie gap jako odstep miedzy statkami

--- ship.py
+++ ship.py
@@ -7,12 +7,12 @@ class Ship:
         self.orientation = orientation
         self.size = 50
 
-    def draw(self, display):
+    def draw(self, display, gap_x, gap_y):
         x_position, y_position = self.position
         vector_x, vector_y = (1, 0) if self.orientation == "hor" else (0, 1)
         for i in range(self.number_of_segment):
-            x = (x_position + vector_x * i) * self.size
-            y = (y_position + vector_y * i) * self.size
+            x = (x_position + vector_x * i) * (self.size + gap_x) 
+            y = (y_position + vector_y * i) * (self.size + gap_y) 
             rectangle = pg.Rect(x, y, self.size, self.size)
             pg.draw.rect(display, (0, 255, 0), rectangle)
             pg.draw.rect(display, (0, 100, 200), rectangle, 1)
